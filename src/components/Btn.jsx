333b14a2c64799a12b1be933fbd980bbb50265c6
marvin obig <obig.marv@gmail.com>
2022-03-14 10:17:01 +0000

    added btn component

---
+++ src/components/Btn.jsx
@@ -0,0 +1,7 @@
+export default function Btn() {
+    return (
+        <div id="btn-parent">
+            <button>Get a new meme image <i className="material-icons"></i></button>
+        </div>
+    )
+}
