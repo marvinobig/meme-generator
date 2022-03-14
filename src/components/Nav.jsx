7c7f28bd6d536401196585a017b4861f8d832f47
marvin obig <obig.marv@gmail.com>
2022-03-14 09:37:28 +0000

    added nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,8 @@
+export default function Nav() {
+    return (
+        <nav id="nav-bar">
+            <i className="material-icons">public</i>
+            <h1>Meme Generator</h1>
+        </nav>
+    )
+}
