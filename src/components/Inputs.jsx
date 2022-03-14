2957bc8c06f2f4e0bb423e5bd259f52b4422ac65
marvin obig <obig.marv@gmail.com>
2022-03-14 10:14:51 +0000

    added input components

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,11 @@
+export default function Inputs() {
+    return (
+        <section className="input-container">
+            <div id="row-1">
+                <input id="input-1" type="text" />
+                <input id="input-2" type="text" />
+            </div>
+            
+        </section>
+    )
+}
