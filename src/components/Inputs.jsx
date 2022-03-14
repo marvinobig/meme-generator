060886c73f2fd5e700a0ca8439af302ae6238e46
marvin obig <obig.marv@gmail.com>
2022-03-14 10:34:56 +0000

    changed class for input container to id

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -2,7 +2,7 @@ import Btn from "./Btn"
 
 export default function Inputs() {
     return (
-        <section className="input-container">
+        <section id="input-container">
             <div id="row-1">
                 <input id="top-text" type="text" />
                 <input id="bottom-text" type="text" />
