4b6328ac293eab0c7f9dbccc51d243d6b03cb938
marvin obig <obig.marv@gmail.com>
2022-03-14 11:24:19 +0000

    added alt tag for troll image

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ export default function Nav() {
     return (
         <nav id="nav-bar">
             <div id="left">
-                <img src={"../media/troll-face.png"} alt="" id="troll" />
+                <img src={"../media/troll-face.png"} alt="troll face" id="troll" />
                 <h1>Marvin Obig</h1>
             </div>
             <h1>Meme Generator</h1>
