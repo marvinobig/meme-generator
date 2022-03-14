ec7e32ecd8f8a77d4422f3b620df6949f5379f99
marvin obig <obig.marv@gmail.com>
2022-03-14 10:12:31 +0000

    added h1 text to nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,10 @@
 export default function Nav() {
     return (
         <nav id="nav-bar">
-            <i className="material-icons">public</i>
+            <div id="left">
+                <i className="material-icons">public</i>
+                <h1>Marvin Obig</h1>
+            </div>
             <h1>Meme Generator</h1>
         </nav>
     )
