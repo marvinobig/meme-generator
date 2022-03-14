62ab36d9028d45899b47a5d1c0287b47c4953772
marvin obig <obig.marv@gmail.com>
2022-03-14 11:23:38 +0000

    added troll-face image

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ export default function Nav() {
     return (
         <nav id="nav-bar">
             <div id="left">
-                <i className="material-icons">public</i>
+                <img src={"../media/troll-face.png"} alt="" id="troll" />
                 <h1>Marvin Obig</h1>
             </div>
             <h1>Meme Generator</h1>
