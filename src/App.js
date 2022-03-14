369cc7052e4b0055b0eb320c97ee6fbbe9e588d2
marvin obig <obig.marv@gmail.com>
2022-03-14 11:29:39 +0000

    added and imported display component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Nav from "./components/Nav";
 import Inputs from "./components/Inputs";
+import Display from "./components/Display";
 import './App.css';
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <div className="App">
       <Nav />
       <Inputs />
+      <Display />
     </div>
   );
 }
