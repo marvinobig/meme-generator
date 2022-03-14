c30d1b63e6ad247966d04b07107c6c8506ea2041
marvin obig <obig.marv@gmail.com>
2022-03-14 10:15:03 +0000

    imported input components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from "./components/Nav";
+import Inputs from "./components/Inputs";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <Nav />
+      <Inputs />
     </div>
   );
 }
