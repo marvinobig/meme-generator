f629ee3deb9cbd2f36dd3d1212720875a2bed615
marvin obig <obig.marv@gmail.com>
2022-03-14 10:30:50 +0000

    added id to button tag in button component

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,7 +1,7 @@
 export default function Btn() {
     return (
         <div id="btn-parent">
-            <button>Get a new meme image <i className="material-icons"></i></button>
+            <button id="get-meme">Get a new meme image <i className="material-icons"></i></button>
         </div>
     )
 }
