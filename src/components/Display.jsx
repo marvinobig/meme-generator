0b23677109c8cdcb9175fdae9129f7e1e0d0991c
marvin obig <obig.marv@gmail.com>
2022-03-16 11:19:35 +0000

    reordered display text and imported image variable that contains url for the image tag

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,9 +1,11 @@
-export default function Display(props) {
+import { image } from "./Btn"
+
+export default function Display() {
     return (
         <section id="display-container">
-            <img src={props.image} alt="meme" />
-            <h1 className="top-meme-text">{props.topText}</h1>
-            <h1 className="bottom-meme-text">{props.bottomText}</h1>
+            <h1>Hello</h1>
+            <img src={`${image}`} alt="meme" />
+            <h1>Mate</h1>
         </section>
     )
 }
