32d20e59828c27eec430ab291592a3bf0fb023a5
marvin obig <obig.marv@gmail.com>
2022-03-16 11:18:39 +0000

    imported meme data object and added function for getting image url

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,7 +1,19 @@
-export default function Btn() {
+import { MEME_DATA } from "../memeData";
+
+let image;
+
+function memeChoice() {
+    let num = Math.floor(Math.random() * 101);
+    image = MEME_DATA.data.memes[num].url;
+    console.log(image)
+}
+
+function Btn() {
     return (
         <div id="btn-parent">
-            <button id="get-meme">Get a new meme image <i className="material-icons">image</i></button>
+            <button onClick={memeChoice} id="get-meme">Get a new meme image <i className="material-icons">image</i></button>
         </div>
     )
 }
+
+export { Btn, image};
