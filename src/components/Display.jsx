728781f7c275ec28b2bf16560cf5208a16402d68
marvin obig <obig.marv@gmail.com>
2022-03-17 19:44:42 +0000

    added button to display component

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,27 @@
-import { image } from "./Btn"
+import React from "react";
+import { MEME_DATA } from "../memeData";
 
 export default function Display() {
+    const [url, setURL] = React.useState("");
+
+    function memeChoice() {
+            const memeArray = MEME_DATA.data.memes;
+            let randomNum = Math.floor(Math.random() * 101);
+            
+            setURL(memeArray[randomNum].url);
+    }
+
     return (
-        <section id="display-container">
-            <h1>Hello</h1>
-            <img src={`${image}`} alt="meme" />
-            <h1>Mate</h1>
-        </section>
+        <div>
+            <div id="btn-parent">
+                <button onClick={memeChoice} id="get-meme">Get a new meme image <i className="material-icons">image</i></button>
+            </div>
+            <section id="display-container">
+                <h1>Hello</h1>
+                <img src={`${url}`} alt="meme" />
+                <h1>Mate</h1>
+            </section>
+        </div>
+        
     )
 }
